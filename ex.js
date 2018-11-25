4e83bb4721f340cfe662785274a8b81760cb446a
hyunmj <35400294+hyunmj@users.noreply.github.com>
2018-11-25 15:22:25 +0900

    Add files via upload

--- ex.js
+++ ex.js
@@ -3,11 +3,14 @@
 var VSHADER_SOURCE =
 'attribute vec4 a_Position;\n' +
 'attribute vec4 a_Normal;\n' +
+'attribute vec2 a_TexCoord;\n' +
 'uniform mat4 u_MvpMatrix;\n' +
 'uniform mat4 u_NormalMatrix;\n' +
+'varying vec2 v_TexCoord;\n' +
 'varying vec4 v_Color;\n' +
 'void main() {\n' +
 ' gl_Position = u_MvpMatrix * a_Position;\n' +
+' v_TexCoord = a_TexCoord;\n' +
 // Shading calculation to make the arm look three-dimensional
 ' vec3 lightDirection = normalize(vec3(0.7, -0.2, 0.2));\n' + // Light direction
 ' vec4 color = vec4(1.0, 0.4, 0.0, 1.0);\n' + // Robot color
@@ -20,9 +23,11 @@ var FSHADER_SOURCE =
 '#ifdef GL_ES\n' +
 'precision mediump float;\n' +
 '#endif\n' +
+'uniform sampler2D u_Sampler;\n' +
+'varying vec2 v_TexCoord;\n' +
 'varying vec4 v_Color;\n' +
 'void main() {\n' +
-' gl_FragColor = v_Color;\n' +
+' gl_FragColor = v_Color * texture2D(u_Sampler, v_TexCoord);\n' +
 '}\n';
 
 function main() {
@@ -43,6 +48,18 @@ function main() {
   var n = initVertexBuffers(gl);
   if (n < 0) {
     console.log('Failed to set the vertex information');
+    return;
+  }
+
+  var tn = initTextureBuffers(gl);
+  if (tn<0) {
+    console.log('Failed to set the texture information');
+    return;
+  }
+
+  // Set texture
+  if (!initTextures(gl, n)) {
+    console.log('Failed to intialize the texture.');
     return;
   }
   // Set the clear color and enable the depth test
@@ -68,6 +85,7 @@ function main() {
   document.onkeydown = function(ev){ keydown(ev, gl, n, viewProjMatrix, u_MvpMatrix,
   u_NormalMatrix); };
   draw(gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix, canvas); // Draw the robot arm
+
 }
 
 var rot=0.0, tran=0.0, g_z=0.0;
@@ -122,6 +140,97 @@ function keydown(ev, gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix) {
   // Draw the robot arm
   draw(gl, n, viewProjMatrix, u_MvpMatrix, u_NormalMatrix);
 }
+
+function initTextureBuffers(gl) {
+  var verticesTexCoords = new Float32Array([
+    // Vertex coordinates, texture coordinate
+    -0.5,  0.5,   0.0, 1.0,
+    -0.5, -0.5,   0.0, 0.0,
+     0.5,  0.5,   1.0, 1.0,
+     0.5, -0.5,   1.0, 0.0,
+  ]);
+  var n = 4; // The number of vertices
+
+  // Create the buffer object
+  var vertexTexCoordBuffer = gl.createBuffer();
+  if (!vertexTexCoordBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+
+  var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+  //Get the storage location of a_Position, assign and enable buffer
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+  gl.enableVertexAttribArray(a_Position);  // Enable the assignment of the buffer object
+
+  // Get the storage location of a_TexCoord
+  var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+  if (a_TexCoord < 0) {
+    console.log('Failed to get the storage location of a_TexCoord');
+    return -1;
+  }
+  // Assign the buffer object to a_TexCoord variable
+  gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+  gl.enableVertexAttribArray(a_TexCoord);  // Enable the assignment of the buffer object
+
+  return n;
+}
+
+function initTextures(gl, n) {
+  var texture = gl.createTexture();   // Create a texture object
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+
+  // Get the storage location of u_Sampler
+  var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+  if (!u_Sampler) {
+    console.log('Failed to get the storage location of u_Sampler');
+    return false;
+  }
+  var image = new Image();  // Create the image object
+  if (!image) {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function(){ loadTexture(gl, n, texture, u_Sampler, image); };
+  // Tell the browser to load an image
+  image.src = 'hyunmj.github.io/a/map.jpg';
+
+  return true;
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Enable texture unit0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Set the texture image
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+  // Set the texture unit 0 to the sampler
+  gl.uniform1i(u_Sampler, 0);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+}
+
 function initVertexBuffers(gl) {
 // Coordinates竊뉱ube which length of one side is 1 with the origin on the center of the bottom)
   var vertices = new Float32Array([
